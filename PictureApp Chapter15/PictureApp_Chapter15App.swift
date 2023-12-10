d38a6e193e2c4bd37a23627036249cd28dbd6620
Freddy <fredericdouamba2014@gmail.com>
2023-12-10 09:12:17 -0800

    Initial Commit

---
+++ PictureApp Chapter15/PictureApp_Chapter15App.swift
@@ -0,0 +1,17 @@
+//
+//  PictureApp_Chapter15App.swift
+//  PictureApp Chapter15
+//
+//  Created by Student Account on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PictureApp_Chapter15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
