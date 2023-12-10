a66430f2d6d527adb332db787161b9e4080fd797
Frederic789 <113332830+Frederic789@users.noreply.github.com>
2023-12-10 10:02:23 -0800

    Update ContentView.swift

--- PictureApp Chapter15/ContentView.swift
+++ PictureApp Chapter15/ContentView.swift
@@ -8,17 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showImagePicker: Bool = false
+    @State private var imageSource: UIImagePickerController.SourceType = .photoLibrary
+    @State private var selectedImage: UIImage?
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let selectedImage = selectedImage {
+                Image(uiImage: selectedImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: 300, maxHeight: 300)
+            }
+            
+            Button("Choose from Photo Library") {
+                self.imageSource = .photoLibrary
+                self.showImagePicker = true
+            }
+            .padding()
+            
+            Button("Take Photo") {
+                self.imageSource = .camera
+                self.showImagePicker = true
+            }
+            .padding()
         }
-        .padding()
+        .sheet(isPresented: $showImagePicker) {
+            ImagePicker(image: self.$selectedImage, sourceType: self.imageSource)
+        }
     }
-}
 
-#Preview {
-    ContentView()
-}
+        struct ImagePicker: UIViewControllerRepresentable {
+            @Environment(\.presentationMode) var presentationMode
+            @Binding var image: UIImage?
+            var sourceType: UIImagePickerController.SourceType
+
+            func makeUIViewController(context: Context) -> UIImagePickerController {
+                let picker = UIImagePickerController()
+                picker.sourceType = sourceType
+                picker.delegate = context.coordinator
+                return picker
+            }
+
+            func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+            func makeCoordinator() -> Coordinator {
+                Coordinator(self)
+            }
+
+            class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+                let parent: ImagePicker
+
+                init(_ parent: ImagePicker) {
+                    self.parent = parent
+                }
+
+                func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+                    if let image = info[.originalImage] as? UIImage {
+                        parent.image = image
+                    }
+
+                    parent.presentationMode.wrappedValue.dismiss()
+                }
+
+                func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+                    parent.presentationMode.wrappedValue.dismiss()
+                }
+            }
+        }
+
+    }
+
+
+
